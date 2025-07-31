ac46dee642d28c07060f1daadcaa278536ad3a18
AnujPatnaik <85904971+AnujPatnaik@users.noreply.github.com>
2025-07-30 19:50:16 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2001,4 +2001,5 @@ def get_location():
     return jsonify({'location': loc})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
